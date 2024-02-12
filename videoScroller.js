483fa70c5e1ec991f81056b189ac9025e954a8d9
Kripa R <kripa@metonymize.com>
2024-02-12 11:07:08 -0600

    fixed typo in videoScroller

--- videoScroller.js
+++ videoScroller.js
@@ -1,4 +1,4 @@
-onsole.clear();
+console.clear();
 /* The encoding is super important here to enable frame-by-frame scrubbing. */
 
 // ffmpeg -i ~/Downloads/Toshiba\ video/original.mov -movflags faststart -vcodec libx264 -crf 23 -g 1 -pix_fmt yuv420p output.mp4
